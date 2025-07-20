f4230f0c643dde4392b51b08a5ce184bdd091e6a
jinseok <jin_114@naver.com>
2025-07-20 15:49:47 +0900

    feat: jpa 코멘트 추가

--- src/main/java/org/example/namelock/repository/CouponRepository.java
+++ src/main/java/org/example/namelock/repository/CouponRepository.java
@@ -1,11 +1,13 @@
 package org.example.namelock.repository;
 
+import jakarta.persistence.QueryHint;
 import org.example.namelock.entity.Coupon;
 import org.example.namelock.entity.CouponStock;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
-    @Query(countName = "회원아이디와 쿠폰 재고로 쿠폰이 있는지 확인")
+    @QueryHints(@QueryHint(name = "org.hibernate.comment", value = "쿠폰 존재 여부 확인"))
     boolean existsByUserIdAndCouponStock(Long userId, CouponStock couponStock);
 }
--- src/main/java/org/example/namelock/repository/CouponStockRepository.java
+++ src/main/java/org/example/namelock/repository/CouponStockRepository.java
@@ -1,10 +1,13 @@
 package org.example.namelock.repository;
 
+import jakarta.persistence.QueryHint;
 import org.example.namelock.entity.CouponStock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.QueryHints;
 
 import java.util.Optional;
 
 public interface CouponStockRepository extends JpaRepository<CouponStock, Long> {
+    @QueryHints(@QueryHint(name = "org.hibernate.comment", value = "쿠폰 코드로 쿠폰 재고 조회"))
     Optional<CouponStock> findByCouponCode(String couponCode);
 }
