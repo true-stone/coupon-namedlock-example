f94869e459dc9b10f9b651fd3cc55ccbb13f036b
jinseok <jin_114@naver.com>
2025-08-02 00:30:38 +0900

    feat: data.sql 중복 실행 방지

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 INSERT INTO coupon_stock (coupon_code, total_issued_quantity, remaining_quantity, created_at, updated_at)
-VALUES ('ABCDEFG', 100, 100, NOW(), NOW());
+VALUES ('ABCDEFG', 100, 100, NOW(), NOW()) ON DUPLICATE KEY
+UPDATE updated_at = NOW();
